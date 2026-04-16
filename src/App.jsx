1444ac6382d4ee968f01d0283b721691014481cb
ahmedjaved753 <ahmedjaved7053@gmail.com>
2026-04-16 09:04:21 +0500

    Fix pages not scrolling to top on navigation
    Add ScrollToTop component that resets scroll position on every
    route change, fixing broken UX where pages appeared mid-scroll.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
@@ -13,6 +14,7 @@ import './App.css';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen flex flex-col">
         <Navigation />
         <main className="flex-1">
